e88f79ae7c53cf973ef226db26038a23918fb470
muradiodev <muradace@gmail.com>
2022-10-03 11:33:32 +0200

    fixed  @Value("${upload-dir}") - import org.springframework.beans.factory.annotation.Value;

--- demo/src/main/java/com/csvtask/demo/config/ApplicationConfig.java
+++ demo/src/main/java/com/csvtask/demo/config/ApplicationConfig.java
@@ -1,7 +1,7 @@
 package com.csvtask.demo.config;
 
 import lombok.Data;
-import lombok.Value;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Data
